82d11415cb9d569ff3f3b66a314ec94784c017a3
Thaina Yu <thainayu@gmail.com>
2016-08-10 12:30:28 +0700

    Update webcam.js

--- webcam.js
+++ webcam.js
@@ -45,9 +45,6 @@ angular.module("TTTT",["ngMaterial"]).controller("TT",($scope) => {
 		$scope.devices = devices.filter((device) => {
 			return device.kind == "videoinput";
 		}).map((device) => device.toJSON());
-		
-		$scope.deviceId	= $scope.devices[$scope.devices.length - 1].deviceId;
-		$scope.switchCamera();
 	}).catch((err) => {
 		console.error(err);
 		$scope.err = err;
